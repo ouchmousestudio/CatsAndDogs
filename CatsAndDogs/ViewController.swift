d95f103a00015da920fca2eaf370bf32663e9daf
Miles <mairuzu@gmail.com>
2019-09-30 02:18:53 +0900

    Initial Commit

---
+++ CatsAndDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatsAndDogs
+//
+//  Created by Miles Fearnall-Williams on 2019/09/30.
+//  Copyright © 2019 Miles Fearnall-Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
